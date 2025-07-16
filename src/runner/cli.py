a51ed4d87dd0e4df49d6254dfa413649af258d74
AiKay96 <kordzadze.ana@gmail.com>
2025-07-16 22:38:30 +0400

    Add scaffold

---
+++ src/runner/cli.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from dotenv import load_dotenv
+from typer import Typer
+
+cli = Typer(no_args_is_help=True, add_completion=False)
+
+@cli.command()
+def run() -> None:
+    load_dotenv()
+    print("Hello Swipe!")
